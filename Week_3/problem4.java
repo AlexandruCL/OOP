2d0ea6dd8dc4305441b6c4c0fa259eca1ab7e5c8
Alexandru CL <laur.cristea@student.upt.ro>
2024-10-16 18:36:56 +0300

    asda

--- Week_3/problem4.java
+++ Week_3/problem4.java
@@ -1,17 +1,17 @@
 class Sum{
-    public static int sum2(int a,int b){
+    public static int sum(int a,int b){
         return a+b;
     }
-    public static int sum3(int a,int b,int c){
-        return sum2(a,b)+c;
+    public static int sum(int a,int b,int c){
+        return sum(a,b)+c;
     }
-    public static int sum4(int a,int b, int c, int d){
-        return sum3(a, b, c)+d;
+    public static int sum(int a,int b, int c, int d){
+        return sum(a, b, c)+d;
     }
     public static void main(String[] args){
-        int sum1=Sum.sum2(3,4);
-        int sum2=Sum.sum3(3,4,5);
-        int sum3=Sum.sum4(3,4,5,6);
+        int sum1=Sum.sum(3,4);
+        int sum2=Sum.sum(3,4,5);
+        int sum3=Sum.sum(3,4,5,6);
         System.out.println(sum1);
         System.out.println(sum2);
         System.out.println(sum3);
