962c31de04530fc4d3c8b3401a31abd1f8c44bca
Alexandru CL <laur.cristea@student.upt.ro>
2024-11-09 03:27:56 +0200

    lab6

--- Week_6/Plane.java
+++ Week_6/Plane.java
@@ -1,15 +1,17 @@
-class Plane{
+abstract class Plane{
     private final int planeId;
     private static int idCounter = 1;
     private final int enginePower;
 
     public Plane(int enginePower){
         this.enginePower = enginePower;
-        this.planeId = idCounter++;
+        this.planeId = idCounter;
+        idCounter++;
     }
 
     public String getPlaneID(){
         return String.valueOf(this.planeId);
+        //return "" + this.planeId;  //Asta e echivalent cu ce e mai sus
     }
 
     public int getTotalEnginePower(){
@@ -29,20 +31,31 @@ class Plane{
     }
 }
 
-class Boeing extends Plane{
-    private final int maxPassengers;
-    public Boeing(int maxPassengers, int powerEngine){
-        super(powerEngine);
+abstract class PassengersPlane extends Plane{
+    private int maxPassengers;
+
+    public PassengersPlane(int maxPassengers, int enginePower){
+        super(enginePower);
         this.maxPassengers = maxPassengers;
     }
-
     public int getMaxPassengers(){
         return this.maxPassengers;
     }
 }
 
-class Concorde extends Boeing{
-    public Concorde(int maxPassengers, int powerEngine){
+class Boeing extends PassengersPlane{
+    public Boeing(int maxPassengers, int powerEngine){
+        super(maxPassengers, powerEngine);
+    }
+}
+
+class Concorde extends PassengersPlane{
+
+    // public Concorde(){
+    //     super(500,1200);                           ////Facem asta ca pentru a atribui default niste valori
+    // }
+
+    public Concorde(int maxPassengers, int powerEngine){              //Asta facem ca sa putem atribui noi valori in main
         super(maxPassengers, powerEngine);
     }
 
@@ -55,7 +68,7 @@ class Concorde extends Boeing{
     }
 }
 
-class FighterPlane extends Plane{
+abstract class FighterPlane extends Plane{
     public FighterPlane(int powerEngine){
         super(powerEngine);
     }
@@ -87,43 +100,96 @@ class TomCat extends FighterPlane{
     }
 }
 
+class Fleet{
+    private static int fleetSize = 100;
+    private int currentpassengersPlanes = 0;
+    private int currentfighterPlanes = 0;
+
+    PassengersPlane[] passengersPlanes = new PassengersPlane[fleetSize];
+    FighterPlane[] fighterPlanes = new FighterPlane[fleetSize];
+
+    public void addPlane(Plane plane){
+        if(plane instanceof PassengersPlane){
+            if(currentpassengersPlanes < fleetSize){
+                passengersPlanes[currentpassengersPlanes] = (PassengersPlane) plane;
+                currentpassengersPlanes++;
+            }
+            else{
+                System.out.println("Passenger Fleet is full");
+            }
+        } else if(plane instanceof FighterPlane){
+            if(currentfighterPlanes < fleetSize){
+                fighterPlanes[currentfighterPlanes] = (FighterPlane) plane;
+                currentfighterPlanes++;
+            }
+            else{
+                System.out.println("Fighter Fleet is full");
+            }
+        }
+    }
+
+    public FighterPlane getgFighterPlane(int index){
+        if(fighterPlanes[index] == null){
+            return null;
+        }
+        return fighterPlanes[index];
+    }
+
+    public Plane getStrongestPlane(){
+        int maxPower = 0;
+        Plane strongestPlane = null;
+        for(int i = 0; i < currentpassengersPlanes; i++){
+            if(passengersPlanes[i].getTotalEnginePower() > maxPower){
+                maxPower = passengersPlanes[i].getTotalEnginePower();
+                strongestPlane = passengersPlanes[i];
+            }
+        }
+        for(int i = 0; i < currentfighterPlanes; i++){
+            if(fighterPlanes[i].getTotalEnginePower() > maxPower){
+                maxPower = fighterPlanes[i].getTotalEnginePower();
+                strongestPlane = fighterPlanes[i];
+            }
+        }
+        return strongestPlane;
+    }
+
+    public int getTotalPassengerCapacity(){
+        int totalPassengerCapacity = 0;
+        for(int i = 0; i < currentpassengersPlanes; i++){
+            totalPassengerCapacity += passengersPlanes[i].getMaxPassengers();
+        }
+        return totalPassengerCapacity;
+    }
+}
+
 class FleetManagement {
     public static void main(String[] args) {
+        Fleet fleet = new Fleet();
 
-        Plane[] fleet = new Plane[8];
+        Boeing boeing1 = new Boeing(300, 1000);
+        Boeing boeing2 = new Boeing(350, 1100);
+        Concorde concorde1 = new Concorde(100, 1500);
+        MIG mig1 = new MIG(2000);
+        TomCat tomcat1 = new TomCat(1800);
 
-        fleet[0] = new Concorde(3000, 100);
-        fleet[1] = new Concorde(3200, 120);
-        fleet[2] = new MIG(2500);
-        fleet[3] = new MIG(2600);
-        fleet[4] = new TomCat(2800);
-        fleet[5] = new TomCat(2900);
-        fleet[6] = new Boeing(28000, 300);
-        fleet[7] = new Boeing(29000, 320);
+        fleet.addPlane(boeing1);
+        fleet.addPlane(boeing2);
+        fleet.addPlane(concorde1);
+        fleet.addPlane(mig1);
+        fleet.addPlane(tomcat1);
 
-        
-        for (Plane plane : fleet) {
-            plane.takeOff();
-            plane.fly();
-            plane.land();
+        System.out.println("Total Passenger Capacity: " + fleet.getTotalPassengerCapacity());
 
-            
-            if (plane instanceof Concorde) {
-                ((Concorde) plane).goSuperSonic();
-                ((Concorde) plane).goSubSonic();
-                System.out.println("Max Passengers: " + ((Concorde) plane).getMaxPassengers());
-            } else if (plane instanceof Boeing){
-                System.out.println("Max Passengers: " + ((Boeing) plane).getMaxPassengers());
-            } else if (plane instanceof MIG) {
-                ((MIG) plane).highSpeedGeometry();
-                ((MIG) plane).normalGeometry();
-                ((FighterPlane) plane).launchMissile();
-            } else if (plane instanceof TomCat) {
-                ((TomCat) plane).refuel();
-                ((FighterPlane) plane).launchMissile();
-            }
-
-            System.out.println("Total Engine Power: " + plane.getTotalEnginePower());
+        Plane strongestPlane = fleet.getStrongestPlane();
+        if (strongestPlane != null) {
+            System.out.println("Strongest Plane ID: " + strongestPlane.getPlaneID() + " with power: " + strongestPlane.getTotalEnginePower());
         }
+
+        concorde1.goSuperSonic();
+        concorde1.goSubSonic();
+        mig1.launchMissile();
+        mig1.highSpeedGeometry();
+        mig1.normalGeometry();
+        tomcat1.refuel();
     }
 }
